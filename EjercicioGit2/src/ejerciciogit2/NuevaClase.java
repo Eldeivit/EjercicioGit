e5cfae3554e5dc6471777d4136fd9cdcd507148e
dam <dam@203P01>
2020-12-21 09:08:02 +0100

    2 punto

---
+++ EjercicioGit2/src/ejerciciogit2/NuevaClase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogit2;
+
+/**
+ *
+ * @author David Hidalgo Aragón
+ */
+public class NuevaClase {
+
+    public static void main(String[] args) {
+        System.out.println("En la rama. Se crea la clase nueva");
+
+    }
+
+}
